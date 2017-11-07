834b7116b1e72bab23db6eabb1f86344afc5c190
Eruvisu <eruvisu@users.noreply.github.com>
2017-11-07 15:28:30 +0200

    Play a message beep using p/Invoke

---
+++ NET Interoperability/Platform Invoke/SimpleInterop/SimpleInterop/PlayMessageBeep.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInterop
+    {
+    class PlayMessageBeep
+    {
+        [DllImport("user32")]
+        extern static bool MessageBeep(uint sound);     //native C function from WinAPI
+
+        static void Main (string[] args)
+        {
+            // Plays a message beep
+            MessageBeep(0x10);
+        }
+        }
+    }
